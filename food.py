6eea062b6b64e45388a4f2bf61d30b58ee81ebe8
HarshithaLakkamraju <125636765+HarshithaLakkamraju@users.noreply.github.com>
2024-05-08 16:38:40 +0530

    Add files via upload

---
+++ food.py
@@ -0,0 +1,12 @@
+import random
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE
+
+class Food:
+    def _init_(self):
+        self.position = self.generate_food_position()
+
+    def generate_food_position(self):
+        # Generate a random position for the food that is within the screen boundaries
+        x = random.randint(0, (SCREEN_WIDTH - GRID_SIZE) // GRID_SIZE) * GRID_SIZE
+        y = random.randint(0, (SCREEN_HEIGHT - GRID_SIZE) // GRID_SIZE) * GRID_SIZE
+        return (x, y)
